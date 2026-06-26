8ca58bf40ad3029acc19601adca289b3d0f441ad
RstK <208619198+bowman025@users.noreply.github.com>
2026-06-26 19:48:32 +0200

    Add refresh token functionality to the auth controller, update prisma schema

--- apps/server/src/controllers/authController.ts
+++ apps/server/src/controllers/authController.ts
@@ -2,7 +2,11 @@ import crypto from 'node:crypto';
 import { db, type User } from '@project-odin-book/db';
 import type { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
-import { signAccessToken, signRefreshToken } from '../auth/jwt.js';
+import {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} from '../auth/jwt.js';
 import { env } from '../config/env.js';
 import { AppError } from '../errors/AppError.js';
 
@@ -76,4 +80,67 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
       }
     },
   )(req, res, next);
+};
+
+export const refresh = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const rawRefreshToken = req.cookies?.refreshToken;
+
+    if (!rawRefreshToken) {
+      throw new AppError('Refresh token missing. Please log in again', 401);
+    }
+
+    const decoded = verifyRefreshToken(rawRefreshToken);
+
+    const hashedTokenFromCookie = crypto
+      .createHash('sha256')
+      .update(rawRefreshToken)
+      .digest('hex');
+
+    const user = await db.user.findUnique({
+      where: { id: decoded.id },
+      select: { id: true, username: true, email: true, refreshToken: true },
+    });
+
+    if (!user) {
+      res.clearCookie('refreshToken');
+      throw new AppError(
+        'Session invalid or expired. Please log in again.',
+        401,
+      );
+    }
+
+    const isValid =
+      user?.refreshToken &&
+      user.refreshToken.length === hashedTokenFromCookie.length &&
+      crypto.timingSafeEqual(
+        Buffer.from(user.refreshToken),
+        Buffer.from(hashedTokenFromCookie),
+      );
+
+    if (!isValid) {
+      res.clearCookie('refreshToken');
+      throw new AppError(
+        'Session invalid or expired. Please log in again.',
+        401,
+      );
+    }
+
+    const newAccessToken = signAccessToken({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      accessToken: newAccessToken,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
