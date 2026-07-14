36096e0a89bd0720f9dd9d23bb941f0267a69ae6
RstK <208619198+bowman025@users.noreply.github.com>
2026-07-14 13:25:23 +0200

    Create test setup

---
+++ apps/server/tests/setup.ts
@@ -0,0 +1,33 @@
+import { db } from '@project-odin-book/db';
+import { afterAll, beforeAll, beforeEach } from 'vitest';
+
+beforeAll(async () => {
+  if (process.env.NODE_ENV !== 'test') {
+    throw new Error('Tests must run with NODE_ENV=test');
+  }
+
+  if (process.env.DATABASE_URL !== process.env.TEST_DATABASE_URL) {
+    throw new Error('Test must run on against TEST_DATABASE_URL');
+  }
+
+  await db.$connect();
+});
+
+beforeEach(async () => {
+  await db.$executeRaw`
+    TRUNCATE TABLE
+    "Message",
+    "Participant",
+    "Conversation",
+    "Comment",
+    "Like",
+    "Post",
+    "Follow",
+    "User"
+    CASCADE;
+  `;
+});
+
+afterAll(async () => {
+  await db.$disconnect();
+});
